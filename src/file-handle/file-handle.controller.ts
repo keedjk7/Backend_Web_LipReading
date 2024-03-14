c8d6c049c8ef175531d878d7af18277b4690c970
Jirapat Kaewsongsang <60883386+keedjk7@users.noreply.github.com>
2024-03-14 16:39:29 +0700

    Delete src/file-handle directory
    unuse

--- src/file-handle/file-handle.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { FileHandleService } from './file-handle.service';
-
-@Controller('file-handle')
-export class FileHandleController {
-  constructor(private readonly fileHandleService: FileHandleService) {}
-}
